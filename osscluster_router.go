cb7868b627b971e5c8753c3f72f174eb6ec7c030
ofekshenawa <ofek.shenawa@redis.com>
2025-06-11 00:02:32 +0300

    feat: implement response aggregator for Redis cluster commands

--- osscluster_router.go
+++ osscluster_router.go
@@ -59,17 +59,13 @@ func (c *ClusterClient) routeAndRun(ctx context.Context, cmd Cmder) error {
 		executed = true
 		firstErr = c.processSpecial(ctx, cmd)
 	}
-	// TODO(ndyakov): fix this, this is not correct and it is added just for inspiration
+
+	// Aggregate responses if we have multiple shard results
 	if firstErr != nil {
 		cmd.SetErr(firstErr)
 		return firstErr
 	} else if len(cmds) > 0 {
-		err := cmdsFirstErr(cmds)
-		if err != nil {
-			cmd.SetErr(err)
-			return err
-		}
-		return nil
+		return c.aggregateResponses(ctx, cmd, cmds, pol)
 	}
 	if executed {
 		return nil
@@ -159,4 +155,125 @@ func (c *ClusterClient) processSlotBased(ctx context.Context, cmd Cmder) error {
 		return err
 	}
 	return node.Client.Process(ctx, cmd)
+}
+
+// aggregateResponses aggregates multiple shard responses according to the response policy.
+func (c *ClusterClient) aggregateResponses(ctx context.Context, cmd Cmder, cmds []Cmder, pol *routing.CommandPolicy) error {
+	// Create the appropriate aggregator based on response policy
+	var aggregator routing.ResponseAggregator
+
+	if pol != nil {
+		// Use explicit response policy
+		aggregator = routing.NewResponseAggregator(pol.Response, cmd.Name())
+	} else {
+		// Use default policy based on whether command has keys
+		firstKeyPos := cmdFirstKeyPos(cmd)
+		// Special handling for test commands with negative keyPos
+		if cmd.firstKeyPos() < 0 {
+			firstKeyPos = 0
+		}
+		isKeyed := firstKeyPos > 0
+		aggregator = routing.NewDefaultAggregator(isKeyed)
+	}
+
+	// Add all shard results to the aggregator
+	for _, shardCmd := range cmds {
+		value := routing.ExtractCommandValue(shardCmd)
+		err := aggregator.Add(value, shardCmd.Err())
+		if err != nil {
+			return err
+		}
+	}
+
+	// Get the final aggregated result
+	finalValue, finalErr := aggregator.Finish()
+	if finalErr != nil {
+		cmd.SetErr(finalErr)
+		return finalErr
+	}
+
+	// Set the aggregated value on the original command
+	return c.setCommandValue(cmd, finalValue)
+}
+
+// setCommandValue sets the aggregated value on a command based on its type.
+// This method is necessary because each command type has a strongly-typed SetVal method,
+// but the aggregator returns interface{}. We need type-safe conversion to set the
+// correct typed value on the original command.
+func (c *ClusterClient) setCommandValue(cmd Cmder, value interface{}) error {
+	// Use concrete type assertions for better reliability
+	switch c := cmd.(type) {
+	case *StatusCmd:
+		if str, ok := value.(string); ok {
+			c.SetVal(str)
+			return nil
+		}
+		return fmt.Errorf("cannot set StatusCmd value from %T", value)
+	case *IntCmd:
+		if i, ok := value.(int64); ok {
+			c.SetVal(i)
+			return nil
+		}
+		return fmt.Errorf("cannot set IntCmd value from %T", value)
+	case *BoolCmd:
+		if b, ok := value.(bool); ok {
+			c.SetVal(b)
+			return nil
+		}
+		return fmt.Errorf("cannot set BoolCmd value from %T", value)
+	case *StringCmd:
+		if str, ok := value.(string); ok {
+			c.SetVal(str)
+			return nil
+		}
+		return fmt.Errorf("cannot set StringCmd value from %T", value)
+	case *FloatCmd:
+		if f, ok := value.(float64); ok {
+			c.SetVal(f)
+			return nil
+		}
+		return fmt.Errorf("cannot set FloatCmd value from %T", value)
+	case *SliceCmd:
+		if slice, ok := value.([]interface{}); ok {
+			c.SetVal(slice)
+			return nil
+		}
+		return fmt.Errorf("cannot set SliceCmd value from %T", value)
+	case *StringSliceCmd:
+		if slice, ok := value.([]string); ok {
+			c.SetVal(slice)
+			return nil
+		}
+		return fmt.Errorf("cannot set StringSliceCmd value from %T", value)
+	case *IntSliceCmd:
+		if slice, ok := value.([]int64); ok {
+			c.SetVal(slice)
+			return nil
+		}
+		return fmt.Errorf("cannot set IntSliceCmd value from %T", value)
+	case *MapStringStringCmd:
+		if m, ok := value.(map[string]string); ok {
+			c.SetVal(m)
+			return nil
+		}
+		return fmt.Errorf("cannot set MapStringStringCmd value from %T", value)
+	case *MapStringIntCmd:
+		if m, ok := value.(map[string]int64); ok {
+			c.SetVal(m)
+			return nil
+		}
+		return fmt.Errorf("cannot set MapStringIntCmd value from %T", value)
+	case *MapStringInterfaceCmd:
+		if m, ok := value.(map[string]interface{}); ok {
+			c.SetVal(m)
+			return nil
+		}
+		return fmt.Errorf("cannot set MapStringInterfaceCmd value from %T", value)
+	case *Cmd:
+		// Generic Cmd can accept any value
+		c.SetVal(value)
+		return nil
+	default:
+		return fmt.Errorf("unsupported command type for setting value: %T", cmd)
+	}
 }
